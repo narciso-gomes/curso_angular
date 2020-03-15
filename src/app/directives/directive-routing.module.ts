7ff8bf768ccba1a0894e6d3309408430ee3bdd07
Narciso Gomes <narcisoalmeida25@gmail.com>
2020-03-15 18:08:09 -0300

    ngClass

--- src/app/directives/directive-routing.module.ts
+++ src/app/directives/directive-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 
 const routes: Routes = [
-    { path: 'ngswitch', component: NgSwitchComponent }
+    { path: 'ngswitch', component: NgSwitchComponent },
+    { path: 'ngclass', component: NgClassComponent}
 ];
 
 @NgModule({
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 import { DirectiveRoutingModule } from './directive-routing.module';
+import { NgClassComponent } from './ng-class/ng-class.component';
 
 
 
 @NgModule({
-  declarations: [NgSwitchComponent],
+  declarations: [NgSwitchComponent, NgClassComponent],
   imports: [
     CommonModule,
     DirectiveRoutingModule

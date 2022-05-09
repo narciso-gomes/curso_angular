1f2b443969321d9c3aabd6606c11b9c2e009a2fd
Narciso Gomes <narcisoalmeida25@gmail.com>
2022-05-08 23:50:25 -0300

    ADD: component custom directives

--- src/app/directives/directive-routing.module.ts
+++ src/app/directives/directive-routing.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { NgSwitchComponent } from './ng-switch/ng-switch.component';
-import { NgClassComponent } from './ng-class/ng-class.component';
-import { NgStyleComponent } from './ng-style/ng-style.component';
-import { NgContentComponent } from './ng-content/ng-content.component';
-
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CustomDirectivesComponent } from "./custom-directives/custom-directives.component";
+import { NgClassComponent } from "./ng-class/ng-class.component";
+import { NgContentComponent } from "./ng-content/ng-content.component";
+import { NgStyleComponent } from "./ng-style/ng-style.component";
+import { NgSwitchComponent } from "./ng-switch/ng-switch.component";
 
 const routes: Routes = [
-    { path: 'ngswitch', component: NgSwitchComponent },
-    { path: 'ngclass', component: NgClassComponent},
-    { path: 'ngstyle', component: NgStyleComponent },
-    { path: 'ngcontent', component: NgContentComponent }
+  { path: "ngswitch", component: NgSwitchComponent },
+  { path: "ngclass", component: NgClassComponent },
+  { path: "ngstyle", component: NgStyleComponent },
+  { path: "ngcontent", component: NgContentComponent },
+  { path: "customs", component: CustomDirectivesComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class DirectiveRoutingModule { }
+export class DirectiveRoutingModule {}
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,22 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NgSwitchComponent } from './ng-switch/ng-switch.component';
-import { DirectiveRoutingModule } from './directive-routing.module';
-import { NgClassComponent } from './ng-class/ng-class.component';
-import { NgStyleComponent } from './ng-style/ng-style.component';
-import { FormsModule } from '@angular/forms';
-import { NgContentComponent } from './ng-content/ng-content.component';
-import { ChildComponentComponent } from './ng-content/child-component/child-component.component';
-import { ChildContentCompostoComponent } from './ng-content/child-content-composto/child-content-composto.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NgSwitchComponent } from "./ng-switch/ng-switch.component";
+import { DirectiveRoutingModule } from "./directive-routing.module";
+import { NgClassComponent } from "./ng-class/ng-class.component";
+import { NgStyleComponent } from "./ng-style/ng-style.component";
+import { FormsModule } from "@angular/forms";
+import { NgContentComponent } from "./ng-content/ng-content.component";
+import { ChildComponentComponent } from "./ng-content/child-component/child-component.component";
+import { ChildContentCompostoComponent } from "./ng-content/child-content-composto/child-content-composto.component";
+import { CustomDirectivesComponent } from "./custom-directives/custom-directives.component";
+import { FundoAmareloDirective } from "./custom-directives/directives/fundo-amarelo.directive";
 
 @NgModule({
-  declarations: [NgSwitchComponent, NgClassComponent, NgStyleComponent, NgContentComponent, ChildComponentComponent, ChildContentCompostoComponent],
-  imports: [
-    CommonModule,
-    DirectiveRoutingModule,
-    FormsModule
-  ]
+  declarations: [
+    NgSwitchComponent,
+    NgClassComponent,
+    NgStyleComponent,
+    NgContentComponent,
+    ChildComponentComponent,
+    ChildContentCompostoComponent,
+    CustomDirectivesComponent,
+    FundoAmareloDirective,
+  ],
+  imports: [CommonModule, DirectiveRoutingModule, FormsModule],
 })
-export class DirectivesModule { }
+export class DirectivesModule {}

fa59d41266b3c7c663c81bc90ea74cc5c027dc13
Narciso Gomes <narcisoalmeida25@gmail.com>
2022-05-09 00:03:03 -0300

    UPDATE: use renderer2 in directive

--- src/app/directives/custom-directives/directives/background-color.directive.ts
+++ src/app/directives/custom-directives/directives/background-color.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from "@angular/core";
 
 @Directive({
-  selector: 'p[backgroundColor]'
+  selector: "p[backgroundColor]",
 })
 export class BackgroundColorDirective {
+  constructor(private element: ElementRef, private renderer2: Renderer2) {
+    // Element ref - use with caution - prefer Renderer2
+    // this.element.nativeElement.style.backgroundColor = "beige";
+    // this.element.nativeElement.style.padding = "10px";
 
-  constructor(private element: ElementRef) {
+    this.renderer2.setStyle(
+      this.element.nativeElement,
+      "backgroundColor",
+      "beige"
+    );
 
-    this.element.nativeElement.style.backgroundColor = 'beige';
-    this.element.nativeElement.style.padding = '10px';
+    this.renderer2.setStyle(this.element.nativeElement, "padding", "14px");
   }
-
 }
